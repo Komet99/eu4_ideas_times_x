4933ded68d467587b4855a25de691cc6c9a87d90
Tehxon <63597407+Komet99@users.noreply.github.com>
2022-08-31 21:36:36 +0200

    Add files via upload

--- pdx.py
+++ pdx.py
@@ -3,13 +3,31 @@ import json
 
 
 def load(path):
+    pdx_obj = []
     file = open(path)
     while True:
-        line = file.readline()
-        for character in [*line]:
-            print(character)
-        if not line:
-            break
+        lines = file.readlines()
+        i = 0
+        for i in range(len(lines)):
+            line = lines[i]
+            if [*line][-1] != "#":
+                parts = line.split("=")
+                header = parts[0].strip()
+                values = []
+
+                i += 1
+                while lines[i][0:4] == "    ":
+                    line_v = lines[i][4:]
+                    if [*line_v][-1] != "#":
+                        parts_v = line_v.split("=")
+                        header_v = parts_v[0].strip()
+
+                pdx_obj[i] = [{"header": header,
+                                   "values": [
+
+                ]}]
+            if not line:
+                break
 
 
 load("C:/Program Files (x86)/Steam/steamapps/common/Europa Universalis IV/common/ideas/00_country_ideas.txt")
